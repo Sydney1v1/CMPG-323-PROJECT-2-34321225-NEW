a23fcec20a219ef3beefba0b3b76cdd6f9f24777
Sydney1v1 <bafanakhumalo540@gmail.com>
2022-11-17 20:26:15 +0200

    API Commit

---
+++ SydneyWeb_API/Authentication/ApplicationUser.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+namespace SydneyWeb_API.Authentication
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
---
+++ SydneyWeb_API/Authentication/UserRoles.cs
@@ -0,0 +1,9 @@
+﻿namespace SydneyWeb_API.Authentication
+{
+    public static class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+
+    }
+}
